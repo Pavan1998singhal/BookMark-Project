cef9bc7baad46c75d0d3d305450eb170d56f7f60
Pavan Singhal <60385486+Pavan1998singhal@users.noreply.github.com>
2020-01-28 15:45:25 +0530

    BookMark creation
    Bookmark of any website can be made by using this project and the data regarding the url name and website name will be stored in your local machine

---
+++ bookmarker/js/main.js
@@ -0,0 +1,118 @@
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+//Save Bookmark
+function saveBookmark(e){
+    //Get form values
+
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    var bookmark = {
+        name : siteName,
+        url : siteUrl
+    };
+
+    if(!validateForm(siteName,siteUrl)){
+        return false;
+    }
+
+
+    //console.log(bookmark);
+    /*
+        //local storage test
+
+        localStorage.setItem('test','hello world');
+        console.log(localStorage.getItem('test'));
+        localStorage.removeItem('test');
+        console.log(localStorage.getItem('test'));
+    */
+
+    //Test if bookmark is null
+    if(localStorage.getItem('bookmarks') === null){
+        //Initiatize an array
+        var bookmarks = [];
+        //Add to array
+        bookmarks.push(bookmark);
+
+        //set to local storage
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }else{
+        //Get bookmark from local storage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        //Add bookmark to array
+        bookmarks.push(bookmark);
+        //Re-set back to local storage
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }
+
+    //Re-fetch bookmarks
+    fetchBookmarks();
+
+    //clear form
+    document.getElementById('myForm').reset();
+
+    //Prevent form from submitting
+    e.preventDefault();
+}
+
+//Delete Bookmark
+function deleteBookmark(url){
+    console.log(url);
+     //Get bookmarks from local storage
+     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+    for(var i = 0; i < bookmarks.length; i++){
+        if(bookmarks[i].url == url){
+            //Remove from array
+            bookmarks.splice(i, 1);
+        }
+    }
+    //Re-set back to local storage
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    
+    //Re-fetch bookmarks
+    fetchBookmarks();
+
+}
+
+//Fetch Bookmarks
+function fetchBookmarks(){
+    //Get bookmarks from local storage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    //console.log(bookmarks);
+    //Get output id
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    //Build output
+    bookmarksResults.innerHTML = '';
+    for(var i = 0; i < bookmarks.length ; i++){
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += '<div class = "well">'+
+                                            '<h3>'+name+
+                                            ' <a class = "btn btn-default" target ="_blank" href ="'+url+'">Visit</a> '+
+                                            ' <a onclick="deleteBookmark(\''+url+'\')" class = "btn btn-danger" href ="#">Delete</a> '
+                                                '</h3>'+
+                                                    '</div>';
+    }
+}
+
+//Validate Form
+function validateForm(siteName,siteUrl){
+    if(!siteName || !siteUrl){
+        alert("Please fill the form");
+        return false;
+    }
+
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+
+    if(!siteUrl.match(regex)){
+        alert("Please enter a valid url");
+        return false;
+    }
+
+    return true;
+}
